69fc74dbcd277f60144f94e19242ac39c67c082d
Egill Fridgeirsson <e.fridgeirsson@erasmusmc.nl>
2026-03-09 15:20:02 +0000

    version 6.6.0

--- inst/doc/AddingCustomModels.R
+++ inst/doc/AddingCustomModels.R
@@ -2,33 +2,39 @@
 library(PatientLevelPrediction)
 
 ## ----echo = TRUE, eval=FALSE--------------------------------------------------
-# attr(param, "settings") <- list(
+# settings <- list(
 #   seed = 12,
-#   modelName = "Special classifier"
+#   modelName = "Special classifier",
+#   trainRFunction = 'madeupFit', # this will be called to train the made up model
+#   predictRFunction = 'madeupPrediction', # this will be called to make predictions
+#   varImpRFunction = 'madeupVarImp' # this will be called to get variable importance
 # )
 
 ## ----tidy=FALSE,eval=FALSE----------------------------------------------------
 # setMadeUp <- function(a = c(1, 4, 10), b = 2, seed = NULL) {
 #   # add input checks here...
 # 
-#   param <- split(
-#     expand.grid(
-#       a = a,
-#       b = b
-#     ),
-#     1:(length(a) * length(b))
-#   )
+#   param <- list(
+#     a = a,
+#     b = b
+#     )
 # 
-#   attr(param, "settings") <- list(
+#   settings <- list(
 #     modelName = "Made Up",
 #     requiresDenseMatrix = TRUE,
-#     seed = seed
+#     seed = seed,
+#     trainRFunction = 'madeupFit', # this will be called to train the made up model
+#     predictRFunction = 'madeupPrediction', # this will be called to make predictions
+#     varImpRFunction = 'madeupVarImp', # this will be called to get variable importance
+#     saveToJson = TRUE,
+#     saveType = "file"
 #   )
 # 
 #   # now create list of all combinations:
 #   result <- list(
-#     fitFunction = "fitMadeUp", # this will be called to train the made up model
-#     param = param
+#     fitFunction = "fitBinaryClassifier", # this will be called to train the made up model
+#     param = param,
+#     settings = settings
 #   )
 #   class(result) <- "modelSettings"
 # 
@@ -36,78 +42,25 @@ library(PatientLevelPrediction)
 # }
 
 ## ----tidy=FALSE,eval=FALSE----------------------------------------------------
-# fitMadeUp <- function(trainData, modelSettings, search, analysisId) {
-#   param <- modelSettings$param
+# fitMadeUp <- function(
+#     dataMatrix,
+#     labels,
+#     hyperParameters,
+#     settings
+#     ) {
+#   # set the seed for reproducibility
+#   set.seed(settings$seed)
 # 
-#   # **************** code to train the model here
-#   # trainedModel <- this code should apply each hyper-parameter combination
-#   # (param[[i]]) using the specified search (e.g., cross validation)
-#   #                 then pick out the best hyper-parameter setting
-#   #                 and finally fit a model on the whole train data using the
-#   #                 optimal hyper-parameter settings
-#   # ****************
+#   # add code here to call a function to train the classifier using the data
+#   # for the specified hyperparameters that are a named list in hyperParameters
+#   model <- madeUpModel(
+#     X = dataMatrix,
+#     Y = labels$outcomeCount,
+#     hyperparameter1 = hyperParameters$hyperparameter1,
+#     hyperparameter2 = hyperParameters$hyperparameter2
+#     )
 # 
-#   # **************** code to apply the model to trainData
-#   # prediction <- code to apply trainedModel to trainData
-#   # ****************
-# 
-#   # **************** code to get variable importance (if possible)
-#   # varImp <- code to get importance of each variable in trainedModel
-#   # ****************
-# 
-# 
-#   # construct the standard output for a model:
-#   result <- list(
-#     model = trainedModel,
-#     prediction = prediction, # the train and maybe the cross validation predictions for the trainData
-#     preprocessing = list(
-#       featureEngineering = attr(trainData$covariateData, "metaData")$featureEngineering,
-#       tidyCovariates = attr(trainData$covariateData, "metaData")$tidyCovariateDataSettings,
-#       requireDenseMatrix = attr(param, "settings")$requiresDenseMatrix,
-#     ),
-#     modelDesign = list(
-#       outcomeId = attr(trainData, "metaData")$outcomeId,
-#       targetId = attr(trainData, "metaData")$targetId,
-#       plpDataSettings = attr(trainData, "metaData")$plpDataSettings,
-#       covariateSettings = attr(trainData, "metaData")$covariateSettings,
-#       populationSettings = attr(trainData, "metaData")$populationSettings,
-#       featureEngineeringSettings = attr(trainData$covariateData, "metaData")$featureEngineeringSettings,
-#       prerocessSettings = attr(trainData$covariateData, "metaData")$prerocessSettings,
-#       modelSettings = list(
-#         model = attr(param, "settings")$modelName, # the model name
-#         param = param,
-#         finalModelParameters = param[[bestInd]], # best hyper-parameters
-#         extraSettings = attr(param, "settings")
-#       ),
-#       splitSettings = attr(trainData, "metaData")$splitSettings,
-#       sampleSettings = attr(trainData, "metaData")$sampleSettings
-#     ),
-#     trainDetails = list(
-#       analysisId = analysisId,
-#       developmentDatabase = attr(trainData, "metaData")$cdmDatabaseSchema,
-#       attrition = attr(trainData, "metaData")$attrition,
-#       trainingTime = timeToTrain, # how long it took to train the model
-#       trainingDate = Sys.Date(),
-#       hyperParamSearch = hyperSummary # the hyper-parameters and performance data.frame
-#     ),
-#     covariateImportance = merge(trainData$covariateData$covariateRef, varImp, by = "covariateId") # add variable importance to covariateRef if possible
-#   )
-#   class(result) <- "plpModel"
-#   attr(result, "predictionFunction") <- "madeupPrediction"
-#   attr(result, "modelType") <- "binary"
-#   return(result)
-# }
-
-## ----tidy=FALSE,eval=FALSE----------------------------------------------------
-# madeupPrediction <- function(plpModel, data, cohort) {
-#   # ************* code to do prediction for each rowId in cohort
-#   # predictionValues <- code to do prediction here returning the predicted risk
-#   #               (value) for each rowId in cohort
-#   #**************
-# 
-#   prediction <- merge(cohort, predictionValues, by = "rowId")
-#   attr(prediction, "metaData") <- list(modelType = attr(plpModel, "modelType"))
-#   return(prediction)
+#   return(model)
 # }
 
 ## ----tidy=FALSE,eval=FALSE----------------------------------------------------
@@ -118,24 +71,27 @@ library(PatientLevelPrediction)
 #     seed <- sample(100000, 1)
 #   }
 # 
-#   param <- split(
-#     expand.grid(
+#   param <- list(
 #       a = a,
 #       b = b
-#     ),
-#     1:(length(a) * length(b))
-#   )
+#     )
 # 
-#   attr(param, "settings") <- list(
+#   settings <- list(
 #     modelName = "Made Up",
 #     requiresDenseMatrix = TRUE,
-#     seed = seed
+#     seed = seed,
+#     trainRFunction = 'fitMadeUp',
+#     predictRFunction = 'predictMadeUp',
+#     varImpRFunction = 'varImpMadeUp',
+#     saveToJson = TRUE,
+#     saveType = "file"
 #   )
 # 
 #   # now create list of all combinations:
 #   result <- list(
-#     fitFunction = "fitMadeUp", # this will be called to train the made up model
-#     param = param
+#     fitFunction = "fitBinaryClassifier", # this is an existing wrapper fit function
+#     param = param,
+#     settings = settings
 #   )
 #   class(result) <- "modelSettings"
 # 
@@ -143,165 +99,29 @@ library(PatientLevelPrediction)
 # }
 
 ## ----tidy=FALSE,eval=FALSE----------------------------------------------------
-# fitMadeUp <- function(trainData, modelSettings, search, analysisId) {
+# fitMadeUp <- function(
+#     dataMatrix,
+#     labels,
+#     hyperParameters,
+#     settings
+#     ) {
 #   # set the seed for reproducibility
-#   param <- modelSettings$param
-#   set.seed(attr(param, "settings")$seed)
+#   set.seed(settings$seed)
 # 
-#   # add folds to labels:
-#   trainData$labels <- merge(trainData$labels, trainData$folds, by = "rowId")
-#   # convert data into sparse R Matrix:
-#   mappedData <- toSparseM(trainData, map = NULL)
-#   matrixData <- mappedData$dataMatrix
-#   labels <- mappedData$labels
-#   covariateRef <- mappedData$covariateRef
-# 
-#   # ============= STEP 1 ======================================
-#   # pick the best hyper-params and then do final training on all data...
-#   writeLines("Cross validation")
-#   paramSel <- lapply(
-#     param,
-#     function(x) {
-#       do.call(
-#         madeUpModel,
-#         list(
-#           param = x,
-#           final = FALSE,
-#           data = matrixData,
-#           labels = labels
-#         )
-#       )
-#     }
-#   )
-#   hyperSummary <- do.call(rbind, lapply(paramSel, function(x) x$hyperSum))
-#   hyperSummary <- as.data.frame(hyperSummary)
-#   hyperSummary$auc <- unlist(lapply(paramSel, function(x) x$auc))
-#   paramSel <- unlist(lapply(paramSel, function(x) x$auc))
-#   bestInd <- which.max(paramSel)
-# 
-#   # get cross val prediction for best hyper-parameters
-#   prediction <- param.sel[[bestInd]]$prediction
-#   prediction$evaluationType <- "CV"
-# 
-#   writeLines("final train")
-#   finalResult <- do.call(
-#     madeUpModel,
-#     list(
-#       param = param[[bestInd]],
-#       final = TRUE,
-#       data = matrixData,
-#       labels = labels
+#   # add code here to call a function to train the classifier using the data
+#   # for the specified hyperparameters that are a named list in hyperParameters
+#   model <- madeUpModel(
+#     X = dataMatrix,
+#     Y = labels$outcomeCount,
+#     hyperparameter1 = hyperParameters$hyperparameter1,
+#     hyperparameter2 = hyperParameters$hyperparameter2
 #     )
-#   )
 # 
-#   trainedModel <- finalResult$model
-# 
-#   # prediction risk on training data:
-#   finalResult$prediction$evaluationType <- "Train"
-# 
-#   # get CV and train prediction
-#   prediction <- rbind(prediction, finalResult$prediction)
-# 
-#   varImp <- covariateRef %>% dplyr::collect()
-#   # no feature importance available
-#   vqrImp$covariateValue <- 0
-# 
-#   timeToTrain <- Sys.time() - start
-# 
-#   # construct the standard output for a model:
-#   result <- list(
-#     model = trainedModel,
-#     prediction = prediction,
-#     preprocessing = list(
-#       featureEngineering = attr(trainData$covariateData, "metaData")$featureEngineering,
-#       tidyCovariates = attr(trainData$covariateData, "metaData")$tidyCovariateDataSettings,
-#       requireDenseMatrix = attr(param, "settings")$requiresDenseMatrix,
-#     ),
-#     modelDesign = list(
-#       outcomeId = attr(trainData, "metaData")$outcomeId,
-#       targetId = attr(trainData, "metaData")$targetId,
-#       plpDataSettings = attr(trainData, "metaData")$plpDataSettings,
-#       covariateSettings = attr(trainData, "metaData")$covariateSettings,
-#       populationSettings = attr(trainData, "metaData")$populationSettings,
-#       featureEngineeringSettings = attr(trainData$covariateData, "metaData")$featureEngineeringSettings,
-#       prerocessSettings = attr(trainData$covariateData, "metaData")$prerocessSettings,
-#       modelSettings = list(
-#         model = attr(param, "settings")$modelName, # the model name
-#         param = param,
-#         finalModelParameters = param[[bestInd]], # best hyper-parameters
-#         extraSettings = attr(param, "settings")
-#       ),
-#       splitSettings = attr(trainData, "metaData")$splitSettings,
-#       sampleSettings = attr(trainData, "metaData")$sampleSettings
-#     ),
-#     trainDetails = list(
-#       analysisId = analysisId,
-#       developmentDatabase = attr(trainData, "metaData")$cdmDatabaseSchema,
-#       attrition = attr(trainData, "metaData")$attrition,
-#       trainingTime = timeToTrain, # how long it took to train the model
-#       trainingDate = Sys.Date(),
-#       hyperParamSearch = hyperSummary # the hyper-parameters and performance data.frame
-#     ),
-#     covariateImportance = merge(trainData$covariateData$covariateRef, varImp, by = "covariateId") # add variable importance to covariateRef if possible
-#   )
-#   class(result) <- "plpModel"
-#   attr(result, "predictionFunction") <- "madeupPrediction"
-#   attr(result, "modelType") <- "binary"
-#   return(result)
+#   return(model)
 # }
 
 ## ----tidy=FALSE,eval=FALSE----------------------------------------------------
-# madeUpModel <- function(param, data, final = FALSE, labels) {
-#   if (final == FALSE) {
-#     # add value column to store all predictions
-#     labels$value <- rep(0, nrow(labels))
-#     attr(labels, "metaData") <- list(modelType = "binary")
-# 
-#     foldPerm <- c() # this holds CV aucs
-#     for (index in 1:max(labels$index)) {
-#       model <- madeup::model(
-#         x = data[labels$index != index, ], # remove left out fold
-#         y = labels$outcomeCount[labels$index != index],
-#         a = param$a,
-#         b = param$b
-#       )
-# 
-#       # predict on left out fold
-#       pred <- stats::predict(model, data[labels$index == index, ])
-#       labels$value[labels$index == index] <- pred
-# 
-#       # calculate auc on help out fold
-#       aucVal <- computeAuc(labels[labels$index == index, ])
-#       foldPerm <- c(foldPerm, aucVal)
-#     }
-#     auc <- computeAuc(labels) # overal AUC
-#   } else {
-#     model <- madeup::model(
-#       x = data,
-#       y = labels$outcomeCount,
-#       a = param$a,
-#       b = param$b
-#     )
-# 
-#     pred <- stats::predict(model, data)
-#     labels$value <- pred
-#     attr(labels, "metaData") <- list(modelType = "binary")
-#     auc <- computeAuc(labels)
-#     foldPerm <- auc
-#   }
-# 
-#   result <- list(
-#     model = model,
-#     auc = auc,
-#     prediction = labels,
-#     hyperSum = c(a = a, b = b, fold_auc = foldPerm)
-#   )
-# 
-#   return(result)
-# }
-
-## ----tidy=FALSE,eval=FALSE----------------------------------------------------
-# madeupPrediction <- function(plpModel, data, cohort) {
+# predictMadeUp <- function(plpModel, data, cohort) {
 #   if (class(data) == "plpData") {
 #     # convert
 #     matrixObjects <- toSparseM(
@@ -333,6 +153,29 @@ library(PatientLevelPrediction)
 # 
 #   attr(cohort, "metaData") <- list(modelType = attr(plpModel, "modelType"))
 #   return(cohort)
+# }
+
+## ----tidy=FALSE,eval=FALSE----------------------------------------------------
+# varImpMadeUp <- function(
+#     model,
+#     covariateMap
+#     ) {
+# 
+#   variableImportance <- tryCatch(
+#       {
+#         varImp <- reticulate::py_to_r(model$feature_importances_)
+#         varImp[is.na(varImp)] <- 0
+# 
+#         covariateMap$covariateValue <- unlist(varImp)
+#         covariateMap$included <- 1
+#       },
+#       error = function(e) {
+#         ParallelLogger::logInfo(e)
+#         return(NULL)
+#       }
+#     )
+# 
+#   return(variableImportance)
 # }
 
 ## ----tidy=TRUE,eval=TRUE------------------------------------------------------
