290d17d200b47fbf6d25912e97200d37c1d92ec3
Egill Fridgeirsson <e.fridgeirsson@erasmusmc.nl>
2026-09-21 13:20:02 +0000

    version 6.7.0

--- inst/doc/InstallationGuide.R
+++ inst/doc/InstallationGuide.R
@@ -3,18 +3,8 @@
 # remotes::install_github("OHDSI/PatientLevelPrediction")
 
 ## ----echo = TRUE, message = FALSE, warning = FALSE,tidy=FALSE,eval=FALSE------
+# Sys.setenv(RETICULATE_PYTHON = "/path/to/python")
 # library(PatientLevelPrediction)
-# reticulate::install_miniconda()
-# configurePython(envname = "r-reticulate", envtype = "conda")
-
-## ----eval=FALSE---------------------------------------------------------------
-# usethis::edit_r_profile()
-
-## ----eval=FALSE---------------------------------------------------------------
-# Sys.setenv(PATH = paste("your python bin location", Sys.getenv("PATH"), sep = ":"))
-
-## ----eval=FALSE---------------------------------------------------------------
-# reticulate::conda_list()
 
 ## ----tidy=TRUE,eval=TRUE------------------------------------------------------
 citation("PatientLevelPrediction")
